fe2e8a84107c7097a78dab2f20d8a4cf383f65a6
Dênis André <47681166+denislira@users.noreply.github.com>
2020-07-04 01:42:33 -0300

    pagina Users

--- index.php
+++ index.php
@@ -67,10 +67,25 @@ $app->post("/admin/users/create", function () {
 
 });
 
+$app->get('/admin/users/:iduser/delete', function($iduser){
+	User::verifylogin();
+	
+});
+
 $app->get('/admin/users/:iduser', function($iduser){
-	User::verifylogin(); //verificar se está logado
-	$page = new PageAdmin();
-     $page->setTpl("users-update");
+ 
+   User::verifyLogin();
+ 
+   $user = new User();
+ 
+   $user->get((int)$iduser);
+ 
+   $page = new PageAdmin();
+ 
+   $page ->setTpl("users-update", array(
+        "user"=>$user->getValues()
+    ));
+ 
 });
 
 $app->post('/admin/users/create', function(){
@@ -83,10 +98,7 @@ $app->post('/admin/users/:iduser', function($iduser){
 	
 });
 
-$app->delete('/admin/users/:iduser', function($iduser){
-	User::verifylogin();
-	
-});
+
 
 $app->run();
 
